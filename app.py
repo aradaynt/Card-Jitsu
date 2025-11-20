729e6b4f0dad37532ec4c6063b2b36c6f0c1fa46
Om Sathe <om.sathe@ontariotechu.net>
2025-11-20 17:39:39 -0500

    Server is Running

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from cardjitsu.models import db
+from cardjitsu.models import db, seed_cards
+
 
 def create_app():
     app = Flask(__name__)
@@ -10,6 +11,17 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+        #seeding the cards table once
+        seed_cards()
+
+    #test route 
+    @app.route("/")
+    def index():
+        return "Card-Jitsu backend is running!"
 
     return app
 
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
